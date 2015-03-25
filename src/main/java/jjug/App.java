1f891e5829bf96c45d9273e0ebdb9a944c272994
yukung <yukung.i@gmail.com>
2015-03-25 14:16:59 +0900

    Prepare some library for image detector.

--- src/main/java/jjug/App.java
+++ src/main/java/jjug/App.java
@@ -2,8 +2,13 @@ package jjug;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.function.BiConsumer;
+
+import static org.bytedeco.javacpp.opencv_core.*;
 
 @SpringBootApplication
 @RestController
@@ -15,5 +20,18 @@ public class App {
     @RequestMapping(value = "/")
     String hello() {
         return "Hello World!";
+    }
+}
+
+@Component
+class FaceDetector {
+    public void detectFaces(Mat source, BiConsumer<Mat, Rect> detectAction) {
+
+    }
+}
+
+class FaceTranslator {
+    public static void duker(Mat source, Rect r) {
+
     }
 }
